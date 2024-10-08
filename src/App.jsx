c2f7c65acf504f75170d04c4b192e62d8cbab11a
Shikhar Shukla <shikharshukla99@Shikhars-MacBook-Air.local>
2024-10-08 23:30:14 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="bg-red-700">Hey This is First App</div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Navbar = () => {
+    return (
+       <nav>
+        <ul className="flex">
+            <li>Home</li>
+            <li>Your Tasks</li>
+        </ul>
+       </nav>
+    )
+}
+
+export default Navbar
