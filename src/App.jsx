0818a2affeed46a5f71e96dbe5df4fa5425d7d2f
shikhar <developer@watchyourhealth.com>
2024-10-16 12:36:12 +0530

    added all the remaining functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,48 @@
-import { useState } from "react";
-import "./App.css";
+import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
+  const [showFinished, setshowFinished] = useState(true);
 
-  const handleEdit = () => {};
-  const handleDelete = (e, id) => {
-    let updatedTodos = todos.filter(item => {
-      return item.id !== id; 
+  useEffect(() => {
+    const todoString = localStorage.getItem("todos");
+    if (todoString) {
+      setTodos(JSON.parse(todoString));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (todos.length > 0) {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
+  }, [todos]);
+
+  const toggleFinished = (e) => {
+    setshowFinished(!showFinished);
+  };
+
+  const handleEdit = (e, id) => {
+    let t = todos.filter((i) => i.id === id);
+    setTodo(t[0].todo);
+    let newTodos = todos.filter((item) => {
+      return item.id !== id;
     });
-    setTodos(updatedTodos);
+    setTodos(newTodos);
+  };
+
+  const handleDelete = (e, id) => {
+    let newTodos = todos.filter((item) => {
+      return item.id !== id;
+    });
+    setTodos(newTodos);
   };
 
   const handleAdd = () => {
-    if (todo.trim()) {
-      setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
-      console.log(todos);
-      setTodo("");
-    }
+    setTodos([...todos, { id: uuidv4(), todo, isCompleted: false }]);
+    setTodo("");
   };
 
   const handleChange = (e) => {
@@ -28,20 +50,19 @@ function App() {
   };
 
   const handleCheckbox = (e) => {
-    const id = e.target.name;
-    const updatedTodos = todos.map((item) => {
-      if (item.id === id) {
-        return { ...item, isCompleted: !item.isCompleted };
-      }
-      return item;
+    let id = e.target.name;
+    let index = todos.findIndex((item) => {
+      return item.id === id;
     });
-    setTodos(updatedTodos);
+    let newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    setTodos(newTodos);
   };
 
   return (
     <>
       <Navbar />
-      <div className="container mx-auto my-5 rounded-xl p-5 bg-blue-200 min-h-[80vh]">
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-blue-200 min-h-[90vh] w-1/2">
         <div className="addTodo my-5">
           <h2 className="text-xl font-bold">Add a Todo Item</h2>
           <input
@@ -53,42 +74,54 @@ function App() {
           <button
             onClick={handleAdd}
             className="bg-blue-800 hover:bg-blue-900 text-white p-2 py-1 rounded-md m-6 font-bold mx-6"
+            disabled={todo.length === 0}
           >
-            Add
+            Save
           </button>
         </div>
+        <input
+          type="checkbox"
+          checked={showFinished}
+          onChange={toggleFinished}
+        />{" "}
+        Show Finished
         <h2 className="text-lg font-bold">Your Todos</h2>
         <div className="todos">
-          {todos.map((item) => (
-            <div
-              key={item.id}
-              className="todo flex w-full justify-between items-center bg-blue-100 p-3 border-2 border-blue-300"
-            >
-              <input
-                type="checkbox"
-                checked={item.isCompleted}
-                onChange={handleCheckbox}
-                name={item.id}
-              />
-              <div className={item.isCompleted ? "line-through" : ""}>
-                {item.todo}
+          {todos.length === 0 && <p className="m-5">Nothing to Do!</p>}
+          {todos
+            .filter((item) => showFinished || !item.isCompleted)
+            .map((item) => (
+              <div
+                key={item.id}
+                className="todo flex w-full my-3 justify-between items-center bg-blue-100 p-3 border-2 border-blue-300"
+              >
+                <input
+                  type="checkbox"
+                  checked={item.isCompleted}
+                  onChange={handleCheckbox}
+                  name={item.id}
+                />
+                <div className={item.isCompleted ? "line-through" : ""}>
+                  {item.todo}
+                </div>
+                <div className="buttons">
+                  <button
+                    onClick={(e) => handleEdit(e, item.id)}
+                    className="bg-blue-800 hover:bg-blue-900 text-white p-3 py-1 rounded-md m-6 font-bold mx-1"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={(e) => {
+                      handleDelete(e, item.id);
+                    }}
+                    className="bg-blue-800 hover:bg-blue-900 text-white p-3 py-1 rounded-md m-6 font-bold mx-1"
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
-              <div className="buttons">
-                <button
-                  onClick={handleEdit}
-                  className="bg-blue-800 hover:bg-blue-900 text-white p-3 py-1 rounded-md m-6 font-bold mx-1"
-                >
-                  Edit
-                </button>
-                <button
-                  onClick={(e)=>{handleDelete(e, item.id)}}
-                  className="bg-blue-800 hover:bg-blue-900 text-white p-3 py-1 rounded-md m-6 font-bold mx-1"
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,18 +3,10 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-between bg-blue-900 text-white py-2">
+    <nav className="flex justify-center bg-blue-900 text-white py-2">
       <div className="logo">
-        <span className="font-bold text-xl mx-9">TodoApp</span>
+        <span className="font-bold text-xl mx-9">Todo App</span>
       </div>
-      <ul className="flex gap-8 mx-9">
-        <li className="cursor-pointer hover:font-bold transition-all duration-50">
-          Home
-        </li>
-        <li className="cursor-pointer hover:font-bold transition-all duration-50">
-          Your Tasks
-        </li>
-      </ul>
     </nav>
   );
 };
